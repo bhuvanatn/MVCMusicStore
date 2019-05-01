0cce127b0ad4dd40fb2adc1539aebb78d1017e25
Bhuvana <bhuvanatn@gmail.com>
2019-05-02 08:31:27 +1000

    mvc music store

---
+++ MVCMusicStore/Controllers/StoreController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCMusicStore.ViewModel;
+using MVCMusicStore.Models;
+
+namespace MVCMusicStore.Controllers
+{
+    public class StoreController : Controller
+    {
+        // GET: Store
+        public ActionResult Index()
+        {
+            // Create a list of genres
+            var genres = new List<string> { "Rock", "Jazz", "Country", "Pop", "Disco" };
+
+            // Create our view model
+            var viewModel = new StoreIndexViewModel
+            {
+                NumberOfGenres = genres.Count(),
+                Genres = genres
+            };
+
+            return this.View(viewModel);
+        }
+        public ActionResult Browse(string genre)
+        {
+            var genreModel = new Genre()
+            {
+                Name = genre
+            };
+
+            var albums = new List<Album>()
+            {
+                new Album() { Title = genre + " Album 1" },
+                new Album() { Title = genre + " Album 2" }
+            };
+
+            var viewModel = new StoreBrowseViewModel()
+            {
+                Genre = genreModel,
+                Albums = albums
+            };
+
+            return this.View(viewModel);
+        }
+
+
+        // GET: /Store/Details/5
+
+        public ActionResult Details(int id)
+        {
+            var album = new Album { Title = "Sample Album" };
+
+            return this.View(album);
+        }
+    }
+}
